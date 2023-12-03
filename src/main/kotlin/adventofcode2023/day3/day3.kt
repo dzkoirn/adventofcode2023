4dd4fc177fc6814d040df23bbdedf3c04df35000
Dzmitry Kalianchuk <koirn.dev@gmail.com>
2023-12-03 22:09:16 +0100

    Day 3 started: solved puzzle2

--- src/main/kotlin/adventofcode2023/day3/day3.kt
+++ src/main/kotlin/adventofcode2023/day3/day3.kt
@@ -6,13 +6,51 @@ fun main() {
     val input = readInput("day3")
     println("Day 3")
     println("Puzzle 1: ${puzzle1(input)}")
+    println("Puzzle 2: ${puzzle2(input)}")
 }
 
 fun puzzle1(list: List<CharSequence>): Int {
     return findAllIntersections(list).sumOf { it.toInt() }
 }
 
+fun puzzle2(list: List<CharSequence>): Int {
+    return findGears(list).map { pair -> pair.map { it.toInt() } }
+        .map { (f, s) -> f * s }
+        .sum()
+}
+
 fun findAllIntersections(list: List<CharSequence>): List<String> {
+    return buildList {
+        list.forEachIndexed { indexL, l ->
+            l.forEachIndexed { indexC, c ->
+                if (!c.isDigit() && c != '.') {
+                    addAll(findIntersection(list, indexC, indexL))
+                }
+            }
+        }
+    }
+}
+
+fun findGears(list: List<CharSequence>): List<List<String>> {
+    return buildList {
+        list.forEachIndexed { indexL, l ->
+            l.forEachIndexed { indexC, c ->
+                if(c == '*') {
+                    val intersections = findIntersection(list, indexC, indexL)
+                    if (intersections.size == 2) {
+                        add(intersections)
+                    }
+                }
+            }
+        }
+    }
+}
+
+private fun findIntersection(
+    list: List<CharSequence>,
+    indexC: Int,
+    indexL: Int,
+): List<String> {
     val foundedDigits = mutableListOf<String>()
 
     fun lineSearch(lineIndex: Int, rowIndex: Int) {
@@ -21,7 +59,7 @@ fun findAllIntersections(list: List<CharSequence>): List<String> {
         } else {
             rowIndex
         }
-        if(startC > 0 && list[lineIndex][startC].isDigit()) {
+        if (startC > 0 && list[lineIndex][startC].isDigit()) {
             while (startC - 1 >= 0 && list[lineIndex][startC - 1].isDigit()) {
                 startC--
             }
@@ -31,7 +69,7 @@ fun findAllIntersections(list: List<CharSequence>): List<String> {
         } else {
             rowIndex
         }
-        if(endC < list[lineIndex].lastIndex && list[lineIndex][endC].isDigit()) {
+        if (endC < list[lineIndex].lastIndex && list[lineIndex][endC].isDigit()) {
             while (endC + 1 <= list[lineIndex].lastIndex && list[lineIndex][endC + 1].isDigit()) {
                 endC++
             }
@@ -42,33 +80,28 @@ fun findAllIntersections(list: List<CharSequence>): List<String> {
             .let { foundedDigits.addAll(it) }
     }
 
-    list.forEachIndexed { indexL, l ->
-        l.forEachIndexed { indexC, c ->
-            if(!c.isDigit() && c != '.') {
-                if (indexC > 0 && list[indexL][indexC - 1].isDigit()) {
-                    var indexCC = indexC - 1
-                    while (indexCC >= 0 && list[indexL][indexCC].isDigit()) {
-                        indexCC--
-                    }
-                    list[indexL].substring(indexCC + 1, indexC)
-                        .takeIf { it.isNotEmpty() }?.let { foundedDigits.add(it) }
-                }
-                if (indexC < l.length && list[indexL][indexC + 1].isDigit()) {
-                    var indexCC = indexC + 1
-                    while (indexCC < l.length && list[indexL][indexCC].isDigit()) {
-                        indexCC++
-                    }
-                    list[indexL].substring(indexC + 1, indexCC)
-                        .takeIf { it.isNotEmpty() }?.let { foundedDigits.add(it) }
-                }
-                if (indexL > 0) {
-                    lineSearch(indexL - 1, indexC)
-                }
-                if (indexL < list.lastIndex) {
-                    lineSearch(indexL + 1, indexC)
-                }
-            }
+
+    if (indexC > 0 && list[indexL][indexC - 1].isDigit()) {
+        var indexCC = indexC - 1
+        while (indexCC >= 0 && list[indexL][indexCC].isDigit()) {
+            indexCC--
         }
+        list[indexL].substring(indexCC + 1, indexC)
+            .takeIf { it.isNotEmpty() }?.let { foundedDigits.add(it) }
+    }
+    if (indexC < list[indexL].length && list[indexL][indexC + 1].isDigit()) {
+        var indexCC = indexC + 1
+        while (indexCC < list[indexL].length && list[indexL][indexCC].isDigit()) {
+            indexCC++
+        }
+        list[indexL].substring(indexC + 1, indexCC)
+            .takeIf { it.isNotEmpty() }?.let { foundedDigits.add(it) }
+    }
+    if (indexL > 0) {
+        lineSearch(indexL - 1, indexC)
+    }
+    if (indexL < list.lastIndex) {
+        lineSearch(indexL + 1, indexC)
     }
     return foundedDigits
 }
--- src/test/kotlin/Day3Test.kt
+++ src/test/kotlin/Day3Test.kt
@@ -1,5 +1,7 @@
 import adventofcode2023.day3.findAllIntersections
+import adventofcode2023.day3.findGears
 import adventofcode2023.day3.puzzle1
+import adventofcode2023.day3.puzzle2
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
@@ -58,6 +60,17 @@ class Day3Test {
         assertThat(findAllIntersections(exampleFromInput.lines())).containsExactlyInAnyOrder(
             "518", "34", "739", "525", "855", "708", "478", "878", "569", "981", "474"
         )
+    }
+
+    @Test
+    fun testFindGears() {
+        val result = findGears(exampleInput.lines())
+        assertThat(result.flatten()).containsExactlyInAnyOrder("467", "35", "755", "598")
+    }
+
+    @Test
+    fun testPuzzle2() {
+        assertEquals(467835, puzzle2(exampleInput.lines()))
     }
 
 }
