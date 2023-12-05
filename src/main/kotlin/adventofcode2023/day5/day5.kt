a109aa34167cc0f7db7b7c124436afc57abb81ce
Dzmitry Kalianchuk <koirn.dev@gmail.com>
2023-12-05 19:22:13 +0100

    Day 5: Solved Puzzle 1

--- src/main/kotlin/adventofcode2023/day5/day5.kt
+++ src/main/kotlin/adventofcode2023/day5/day5.kt
@@ -1,31 +1,75 @@
 package adventofcode2023.day5
 
-import java.util.stream.Stream
+import adventofcode2023.readInput
 
 fun main() {
+    val input = readInput("day5")
     println("Day 5")
+    println("Puzzle 1: ${puzzle1(input)}")
 }
 
 data class ParsedInput(
     val seeds: List<Long>,
-    val mappers: List<Map<Long, Long>>,
-)
+    val mappers: List<Mapper>,
+) {
+
+    data class Mapper(
+        val records: List<MapRecord>
+    ) {
+        data class MapRecord(
+            val destinationIndex: Long,
+            val sourceIndex: Long,
+            val range: Int
+        )
+    }
+}
 
 fun parseInput(input: List<String>): ParsedInput {
     val seeds = input.first().split(':')
-        .let { (_, numbers) -> numbers.split(' ')
-            .filter { it.isNotEmpty() }
-            .map { it.toLong() }
+        .let { (_, numbers) ->
+            numbers.split(' ')
+                .filter { it.isNotEmpty() }
+                .map { it.toLong() }
         }
-    val mappers: List<Map<Long, Long>> = buildList {
-        var map: MutableMap<Long, Long> = mutableMapOf()
+    val mappers: List<ParsedInput.Mapper> = buildList {
+        var records: MutableList<ParsedInput.Mapper.MapRecord> = mutableListOf()
         input.drop(2).forEach { line ->
             when {
-                line.isEmpty() -> { add(map) }
-                line.first().isLetter() -> { map = mutableMapOf() }
-                else -> { line.split(' ').filter { it.isNotEmpty() }.map { it.toLong() } }
+                line.isEmpty() -> {
+                    add(ParsedInput.Mapper(records))
+                }
+
+                line.first().isLetter() -> {
+                    records = mutableListOf()
+                }
+
+                else -> {
+                    line.split(' ')
+                        .filter { it.isNotEmpty() }
+                        .map { it.toLong() }
+                        .let { (dI, sI, r) ->
+                            ParsedInput.Mapper.MapRecord(
+                                destinationIndex = dI,
+                                sourceIndex = sI,
+                                range = r.toInt()
+                            )
+                        }.let { records.add(it) }
+                }
             }
         }
+        add(ParsedInput.Mapper(records))
     }
     return ParsedInput(seeds, mappers)
+}
+
+fun ParsedInput.Mapper.map(value: Long): Long {
+    val mapRecord = records.find { value in LongRange(it.sourceIndex, it.sourceIndex + it.range) }
+    return mapRecord?.let { it.destinationIndex + (value - it.sourceIndex) } ?: value
+}
+
+fun puzzle1(input: List<String>): Long {
+    val parsedInput = parseInput(input)
+    return parsedInput.seeds.map { s ->
+        parsedInput.mappers.fold(s) { s, mapper -> mapper.map(s) }
+    }.min()
 }
--- src/test/kotlin/Day5Test.kt
+++ src/test/kotlin/Day5Test.kt
@@ -1,25 +1,214 @@
 import adventofcode2023.day5.ParsedInput
+import adventofcode2023.day5.map
 import adventofcode2023.day5.parseInput
+import adventofcode2023.day5.puzzle1
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.MethodSource
+import java.util.stream.Stream
 
 class Day5Test {
 
-//    @Test
-//    fun testInputParsing() {
-//        val actual = parseInput(inputExample.lines())
-//
-//        val expected = ParsedInput(
-//            seeds = listOf(79, 14, 55, 13),
-//            mappers = listOf(
-//                buildMap {
-//                    add
-//                }
-//                mapOf(98 to 50, 99 to 51)
-//            )
-//        )
-//    }
+    @Test
+    fun testInputParsing() {
+        val actual = parseInput(inputExample.lines())
+
+        val expected = ParsedInput(
+            seeds = listOf(79, 14, 55, 13),
+            mappers = listOf(
+                /*seed-to-soil map:
+                50 98 2
+                52 50 48*/
+                ParsedInput.Mapper(
+                    records = listOf(
+                        ParsedInput.Mapper.MapRecord(
+                            destinationIndex = 50,
+                            sourceIndex = 98,
+                            range = 2
+                        ),
+                        ParsedInput.Mapper.MapRecord(
+                            destinationIndex = 52,
+                            sourceIndex = 50,
+                            range = 48
+                        )
+                    )
+                ),
+                /*soil-to-fertilizer map:
+                0 15 37
+                37 52 2
+                39 0 15*/
+                ParsedInput.Mapper(
+                    records = listOf(
+                        ParsedInput.Mapper.MapRecord(
+                            destinationIndex = 0,
+                            sourceIndex = 15,
+                            range = 37
+                        ),
+                        ParsedInput.Mapper.MapRecord(
+                            destinationIndex = 37,
+                            sourceIndex = 52,
+                            range = 2
+                        ),
+                        ParsedInput.Mapper.MapRecord(
+                            destinationIndex = 39,
+                            sourceIndex = 0,
+                            range = 15
+                        )
+                    )
+                ),
+                /*fertilizer-to-water map:
+                49 53 8
+                0 11 42
+                42 0 7
+                57 7 4*/
+                ParsedInput.Mapper(
+                    records = listOf(
+                        ParsedInput.Mapper.MapRecord(
+                            destinationIndex = 49,
+                            sourceIndex = 53,
+                            range = 8
+                        ),
+                        ParsedInput.Mapper.MapRecord(
+                            destinationIndex = 0,
+                            sourceIndex = 11,
+                            range = 42
+                        ),
+                        ParsedInput.Mapper.MapRecord(
+                            destinationIndex = 42,
+                            sourceIndex = 0,
+                            range = 7
+                        ),
+                        ParsedInput.Mapper.MapRecord(
+                            destinationIndex = 57,
+                            sourceIndex = 7,
+                            range = 4
+                        )
+                    )
+                ),
+                /*water-to-light map:
+                88 18 7
+                18 25 70*/
+                ParsedInput.Mapper(
+                    records = listOf(
+                        ParsedInput.Mapper.MapRecord(
+                            destinationIndex = 88,
+                            sourceIndex = 18,
+                            range = 7
+                        ),
+                        ParsedInput.Mapper.MapRecord(
+                            destinationIndex = 18,
+                            sourceIndex = 25,
+                            range = 70
+                        )
+                    )
+                ),
+                /*light-to-temperature map:
+                45 77 23
+                81 45 19
+                68 64 13*/
+                ParsedInput.Mapper(
+                    records = listOf(
+                        ParsedInput.Mapper.MapRecord(
+                            destinationIndex = 45,
+                            sourceIndex = 77,
+                            range = 23
+                        ),
+                        ParsedInput.Mapper.MapRecord(
+                            destinationIndex = 81,
+                            sourceIndex = 45,
+                            range = 19
+                        ),
+                        ParsedInput.Mapper.MapRecord(
+                            destinationIndex = 68,
+                            sourceIndex = 64,
+                            range = 13
+                        )
+                    )
+                ),
+                /*temperature-to-humidity map:
+                0 69 1
+                1 0 69*/
+                ParsedInput.Mapper(
+                    records = listOf(
+                        ParsedInput.Mapper.MapRecord(
+                            destinationIndex = 0,
+                            sourceIndex = 69,
+                            range = 1
+                        ),
+                        ParsedInput.Mapper.MapRecord(
+                            destinationIndex = 1,
+                            sourceIndex = 0,
+                            range = 69
+                        )
+                    )
+                ),
+                /*humidity-to-location map:
+                60 56 37
+                56 93 4*/
+                ParsedInput.Mapper(
+                    records = listOf(
+                        ParsedInput.Mapper.MapRecord(
+                            destinationIndex = 60,
+                            sourceIndex = 56,
+                            range = 37,
+                        ),
+                        ParsedInput.Mapper.MapRecord(
+                            destinationIndex = 56,
+                            sourceIndex = 93,
+                            range = 4
+                        )
+                    )
+                )
+            )
+        )
+
+        assertEquals(expected, actual)
+    }
+
+    @ParameterizedTest
+    @MethodSource("mapperSource")
+    fun testMapperMap(testCase: Pair<Long, Long>) {
+        val mapper = ParsedInput.Mapper(
+            records = listOf(
+                ParsedInput.Mapper.MapRecord(
+                    destinationIndex = 50,
+                    sourceIndex = 98,
+                    range = 2
+                ),
+                ParsedInput.Mapper.MapRecord(
+                    destinationIndex = 52,
+                    sourceIndex = 50,
+                    range = 48
+                )
+            )
+        )
+        assertEquals(testCase.second, mapper.map(testCase.first))
+    }
+
+    @Test
+    fun puzzle1Test() {
+        assertEquals(35, puzzle1(inputExample.lines()))
+    }
 
     companion object {
+        @JvmStatic
+        fun mapperSource(): Stream<Pair<Long, Long>> = Stream.of(
+            0L to 0L,
+            1L to 1L,
+            2L to 2L,
+            48L to 48L,
+            49L to 49L,
+            50L to 52L,
+            51L to 53L,
+            52L to 54L,
+            66L to 68L,
+            96L to 98L,
+            97L to 99L,
+            98L to 50L,
+            99L to 51L
+        )
+
 
         private val inputExample = """
             seeds: 79 14 55 13
