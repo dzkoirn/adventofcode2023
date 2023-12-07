ce41a2667524c265b71f9c2bf6d1b18f1c0736ae
Dzmitry Kalianchuk <koirn.dev@gmail.com>
2023-12-07 21:46:08 +0100

    Day 7: Solved puzzle 2

--- src/main/kotlin/adventofcode2023/day7/day7.kt
+++ src/main/kotlin/adventofcode2023/day7/day7.kt
@@ -10,6 +10,11 @@ fun main() {
         println("Puzzle 1: ${puzzle1(input)}")
     }
     println("Puzzle 1 took $puzzle1Time")
+
+    val puzzle2Time = measureTime {
+        println("Puzzle 2: ${puzzle2(input)}")
+    }
+    println("Puzzle 2 took $puzzle2Time")
 }
 
 data class Hand(
@@ -20,9 +25,14 @@ data class Hand(
 fun prepareInput(input: List<String>): List<Hand> =
     input.map { it.split(' ').filter { it.isNotEmpty() }.let { (f, s) -> Hand(cards = f, bid = s.toInt()) } }
 
-fun sortHands(input: List<Hand>): List<Hand> = input.sortedWith { first, second -> compareHands(first.cards, second.cards) }
+fun sortHands(
+    input: List<Hand>,
+    toHandType: (String) -> HandType = ::toHandTypeV1,
+    sortedCards: CharArray = sortedCardsv1
+): List<Hand> =
+    input.sortedWith { first, second -> compareHands(first.cards, second.cards, toHandType, sortedCards) }
 
-private val sortedCard = arrayOf('A', 'K', 'Q', 'J', 'T', '9', '8', '7', '6', '5', '4', '3', '2')
+val sortedCardsv1 = charArrayOf('A', 'K', 'Q', 'J', 'T', '9', '8', '7', '6', '5', '4', '3', '2', 'J')
 
 enum class HandType {
     HIGH_CARD,
@@ -34,24 +44,32 @@ enum class HandType {
     FIVE_OF_KIND;
 }
 
-fun compareHands(first: String, second: String): Int {
-    val firstHand = first.toHandType()
-    val secondHand = second.toHandType()
+fun compareHands(
+    first: String,
+    second: String,
+    toHandType: (String) -> HandType,
+    sortedCards: CharArray
+): Int {
+    val firstHand = toHandType(first)
+    val secondHand = toHandType(second)
     var comparison = firstHand.compareTo(secondHand)
     if (comparison == 0) {
         var index = 0
         do {
-            comparison = compareCards(first[index], second[index])
+            comparison = compareCards(first[index], second[index], sortedCards)
             index++
-        } while(comparison == 0 && index < 5)
+        } while (comparison == 0 && index < 5)
     }
     return comparison
 }
 
-fun compareCards(first: Char, second: Char): Int = sortedCard.indexOf(second) - sortedCard.indexOf(first)
+fun compareCards(first: Char, second: Char, sortedCards: CharArray): Int =
+    with(sortedCards) {
+        indexOf(second) - indexOf(first)
+    }
 
-fun String.toHandType(): HandType {
-    val mapped = this.groupBy { it }
+fun toHandTypeV1(hand: String): HandType {
+    val mapped = hand.groupBy { it }
     return when (mapped.keys.size) {
         5 -> HandType.HIGH_CARD
         4 -> HandType.ONE_PAIR
@@ -62,6 +80,7 @@ fun String.toHandType(): HandType {
                 HandType.TWO_PAIR
             }
         }
+
         2 -> {
             if (mapped.values.any { it.size == 4 }) {
                 HandType.FOUR_OF_KIND
@@ -69,6 +88,7 @@ fun String.toHandType(): HandType {
                 HandType.FULL_HOUSE
             }
         }
+
         1 -> HandType.FIVE_OF_KIND
         else -> {
             throw IllegalStateException()
@@ -78,3 +98,29 @@ fun String.toHandType(): HandType {
 
 fun puzzle1(input: List<String>): Long =
     sortHands(prepareInput(input)).foldIndexed(0L) { index: Int, acc: Long, hand: Hand -> acc + hand.bid * (index + 1) }
+
+val sortedCardsv2 = charArrayOf('A', 'K', 'Q', 'T', '9', '8', '7', '6', '5', '4', '3', '2', 'J')
+
+fun toHandTypeV2(hand: String): HandType {
+    val mapped = hand.groupBy { it }
+    if (mapped.keys.size == 1) {
+        return HandType.FIVE_OF_KIND
+    }
+    val tempHand = if (mapped.keys.contains('J')) {
+        val char = mapped.entries.filter { it.key != 'J' }.sortedWith { f, s ->
+            val comparison = f.value.size - s.value.size
+            if (comparison == 0) {
+                compareCards(f.key, s.key, sortedCardsv2)
+            } else {
+                comparison
+            }
+        }.last.key
+        hand.replace('J', char)
+    } else {
+        hand
+    }
+    return toHandTypeV1(tempHand)
+}
+
+fun puzzle2(input: List<String>): Long =
+    sortHands(prepareInput(input), ::toHandTypeV2, sortedCardsv2).foldIndexed(0L) { index: Int, acc: Long, hand: Hand -> acc + hand.bid * (index + 1) }
--- src/test/kotlin/Day7Test.kt
+++ src/test/kotlin/Day7Test.kt
@@ -4,27 +4,29 @@ import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
 import org.junit.jupiter.params.provider.MethodSource
+import java.util.stream.Stream
 
 class Day7Test {
 
     @ParameterizedTest
     @MethodSource("comparisonCards")
-    fun testCardsComparator(testCase: Triple<Char, Char, (Int)->Boolean>) {
-        val actual = compareCards(testCase.first, testCase.second)
-        assertTrue( { testCase.third(actual) }, "actual = $actual")
+    fun testCardsComparator(testCase: Triple<Char, Char, (Int) -> Boolean>) {
+        val actual = compareCards(testCase.first, testCase.second, sortedCardsv1)
+        assertTrue({ testCase.third(actual) }, "actual = $actual")
     }
 
     @ParameterizedTest
     @MethodSource("handsType")
     fun testToHandsTypeFunction(testCase: Pair<String, HandType>) {
-        assertEquals(testCase.second, testCase.first.toHandType())
+        assertEquals(testCase.second, toHandTypeV1(testCase.first))
     }
 
     @ParameterizedTest
     @MethodSource("comparisonTest")
-    fun testHandsComparator(testCase: Triple<String, String, (Int)->Boolean>) {
-        val actual = compareHands(testCase.first, testCase.second)
+    fun testHandsComparator(testCase: Triple<String, String, (Int) -> Boolean>) {
+        val actual = compareHands(testCase.first, testCase.second, ::toHandTypeV1, sortedCardsv1)
         assertTrue({ testCase.third(actual) }, "actual = $actual")
     }
 
@@ -45,13 +47,55 @@ class Day7Test {
         assertEquals(6440, puzzle1(inputExample.lines()))
     }
 
+    @ParameterizedTest
+    @MethodSource("comparisonCardsv2")
+    fun testCardsComparatorV2(testCase: Triple<Char, Char, (Int) -> Boolean>) {
+        val actual = compareCards(testCase.first, testCase.second, sortedCardsv2)
+        assertTrue({ testCase.third(actual) }, "actual = $actual")
+    }
+
+    @ParameterizedTest
+    @MethodSource("comparisonTestV2")
+    fun testHandsComparatorV2(testCase: Triple<String, String, (Int) -> Boolean>) {
+        val actual = compareHands(testCase.first, testCase.second, ::toHandTypeV2, sortedCardsv2)
+        assertTrue({ testCase.third(actual) }, "actual = $actual")
+    }
+
+    @ParameterizedTest
+    @MethodSource("handsTypeV2")
+    fun testToHandsTypeFunctionV2(testCase: Pair<String, HandType>) {
+        assertEquals(testCase.second, toHandTypeV2(testCase.first))
+    }
+
+    @Test
+    fun testSortInputV2() {
+        assertThat(
+            sortHands(
+                prepareInput(inputExample.lines()),
+                ::toHandTypeV2,
+                sortedCardsv2
+            )
+        ).containsExactly(
+            Hand("32T3K", 765),
+            Hand("KK677", 28),
+            Hand("T55J5", 684),
+            Hand("QQQJA", 483),
+            Hand("KTJJT", 220)
+        )
+    }
+
+    @Test
+    fun testPuzzle2() {
+        assertEquals(5905, puzzle2(inputExample.lines()))
+    }
+
     companion object {
         @JvmStatic
         fun comparisonTest() = listOf(
-            Triple("77888", "77788") { v: Int -> v > 0},
-            Triple("32T3K", "T55J5") { v: Int -> v < 0},
-            Triple("32T3K", "32T3K") { v: Int -> v == 0},
-            Triple("KK677", "KTJJT") { v: Int -> v > 0}
+            Triple("77888", "77788") { v: Int -> v > 0 },
+            Triple("32T3K", "T55J5") { v: Int -> v < 0 },
+            Triple("32T3K", "32T3K") { v: Int -> v == 0 },
+            Triple("KK677", "KTJJT") { v: Int -> v > 0 }
         )
 
         @JvmStatic
@@ -72,7 +116,7 @@ class Day7Test {
         )
 
         @JvmStatic
-        fun handsType()= listOf(
+        fun handsType() = listOf(
             "AAAAA" to HandType.FIVE_OF_KIND,
             "AA8AA" to HandType.FOUR_OF_KIND,
             "23332" to HandType.FULL_HOUSE,
@@ -80,6 +124,46 @@ class Day7Test {
             "23432" to HandType.TWO_PAIR,
             "A23A4" to HandType.ONE_PAIR,
             "23456" to HandType.HIGH_CARD
+        )
+
+        @JvmStatic
+        fun comparisonCardsv2() = listOf(
+            Triple('A', 'K') { v: Int -> v > 0 },
+            Triple('K', 'Q') { v: Int -> v > 0 },
+            Triple('Q', 'J') { v: Int -> v > 0 },
+            Triple('J', 'T') { v: Int -> v < 0 },
+            Triple('9', 'T') { v: Int -> v < 0 },
+            Triple('9', '8') { v: Int -> v > 0 },
+            Triple('7', '8') { v: Int -> v < 0 },
+            Triple('7', '6') { v: Int -> v > 0 },
+            Triple('5', '6') { v: Int -> v < 0 },
+            Triple('5', '4') { v: Int -> v > 0 },
+            Triple('3', '4') { v: Int -> v < 0 },
+            Triple('3', '2') { v: Int -> v > 0 },
+            Triple('2', '2') { v: Int -> v == 0 },
+            Triple('2', 'J') { v: Int -> v > 0 },
+        )
+
+        @JvmStatic
+        fun comparisonTestV2() = listOf(
+            Triple("77888", "77788") { v: Int -> v > 0 },
+            Triple("32T3K", "T55J5") { v: Int -> v < 0 },
+            Triple("32T3K", "32T3K") { v: Int -> v == 0 },
+            Triple("KK677", "KTJJT") { v: Int -> v < 0 }
+        )
+
+        @JvmStatic
+        fun handsTypeV2() = listOf(
+            "AAAAA" to HandType.FIVE_OF_KIND,
+            "AA8AA" to HandType.FOUR_OF_KIND,
+            "23332" to HandType.FULL_HOUSE,
+            "TTT98" to HandType.THREE_OF_KIND,
+            "23432" to HandType.TWO_PAIR,
+            "A23A4" to HandType.ONE_PAIR,
+            "23456" to HandType.HIGH_CARD,
+            "T55J5" to HandType.FOUR_OF_KIND,
+            "KTJJT" to HandType.FOUR_OF_KIND,
+            "QQQJA" to HandType.FOUR_OF_KIND,
         )
 
         val inputExample = """
