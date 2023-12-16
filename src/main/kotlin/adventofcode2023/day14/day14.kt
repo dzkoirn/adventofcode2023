9c4a8c1d693c225d1813bd01df1ed76e8af37b36
Dzmitry Kalianchuk <koirn.dev@gmail.com>
2023-12-16 23:32:11 +0100

    Day 14: puzzle 2 - solved

--- src/main/kotlin/adventofcode2023/day14/day14.kt
+++ src/main/kotlin/adventofcode2023/day14/day14.kt
@@ -8,28 +8,33 @@ fun main() {
     val input = readInput("day14")
     println("Puzzle 1")
     val time1 = measureTime {
-        println(calculateLoad(moveRocks(input)))
+        println(calculateLoad(moveRocks(input.map { it.toCharArray() })))
     }
     println("puzzle 1 took $time1")
+
+    println("Puzzle 2")
+    val time2 = measureTime {
+        println(calculateLoad(circle(input, 1000000000)))
+    }
+    println("puzzle 2 took $time2")
 }
 
-fun moveRocks(input: List<String>): List<CharArray> {
-    val newInput = input.map { it.toCharArray() }
+fun moveRocks(input: List<CharArray>): List<CharArray> {
     for (line in (1..input.lastIndex)) {
         input[line].indices.forEach { column ->
-            if (newInput[line][column] == 'O') {
+            if (input[line][column] == 'O') {
                 var newLine = line
-                while (newLine > 0 && newInput[newLine - 1][column] == '.') {
+                while (newLine > 0 && input[newLine - 1][column] == '.') {
                     newLine--
                 }
                 if (newLine != line) {
-                    newInput[newLine][column] = 'O'
-                    newInput[line][column] = '.'
+                    input[newLine][column] = 'O'
+                    input[line][column] = '.'
                 }
             }
         }
     }
-    return newInput
+    return input
 }
 
 fun calculateLoad(input: List<CharArray>) : Int {
@@ -38,4 +43,43 @@ fun calculateLoad(input: List<CharArray>) : Int {
         counter += chars.count { it == 'O' } * (input.size - index)
     }
     return counter
+}
+
+fun List<CharArray>.rotate(): List<CharArray> {
+    /*
+        1 2 3     7 4 1
+        4 5 6 =>  8 5 2
+        7 8 9     9 6 3
+     */
+    return buildList {
+        this@rotate.first.indices.forEach { index ->
+            buildList {
+                this@rotate.reversed().forEach { arr ->
+                    add(arr[index])
+                }
+            }.toCharArray().let { add(it) }
+        }
+    }
+}
+
+fun circle(input: List<String>, n: Int): List<CharArray> {
+    var preparedInput = input.map { it.toCharArray() }
+    val cache = mutableMapOf<String, Int>()
+    val cache2 = mutableMapOf<Int, List<CharArray>>()
+    repeat(n) { iteration ->
+        val buildString = preparedInput.joinToString("\n") { chars -> chars.joinToString("") { "$it" } }
+        val oldValue = cache.put(buildString, iteration)
+        if (oldValue != null) {
+            val diff = iteration - oldValue
+            val newIndex = oldValue + (n - oldValue) % diff
+            return cache2[newIndex]!!
+        }
+        cache2[iteration] = buildList { addAll(preparedInput.map { it.copyOf() }) }
+        val p1 = moveRocks(preparedInput)
+        val p2 = moveRocks(p1.rotate())
+        val p3 = moveRocks(p2.rotate())
+        preparedInput = moveRocks(p3.rotate()).rotate()
+        println("iteration $iteration")
+    }
+    return preparedInput
 }
--- src/test/kotlin/Day14Test.kt
+++ src/test/kotlin/Day14Test.kt
@@ -1,14 +1,18 @@
 import adventofcode2023.day14.calculateLoad
+import adventofcode2023.day14.circle
 import adventofcode2023.day14.moveRocks
+import adventofcode2023.day14.rotate
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.MethodSource
 
 class Day14Test {
 
     @Test
     fun testMoveRocks() {
-        assertThat(moveRocks(exampleInput)).containsExactly(
+        assertThat(moveRocks(exampleInput.map { it.toCharArray() })).containsExactly(
             charArrayOf('O', 'O', 'O', 'O', '.', '#', '.', 'O', '.', '.'),
             charArrayOf('O', 'O', '.', '.', '#', '.', '.', '.', '.', '#'),
             charArrayOf('O', 'O', '.', '.', 'O', '#', '#', '.', '.', 'O'),
@@ -22,12 +26,79 @@ class Day14Test {
         )
     }
 
+    @ParameterizedTest
+    @MethodSource("circle")
+    fun testCircle(testCase: Pair<Int, List<CharArray>>) {
+        assertThat(circle(exampleInput, testCase.first)).containsExactly(
+            *testCase.second.toTypedArray()
+        )
+    }
+
     @Test
     fun testCalculateLoad() {
-        assertEquals(136, calculateLoad(moveRocks(exampleInput)))
+        assertEquals(136, calculateLoad(moveRocks(exampleInput.map { it.toCharArray() })))
+    }
+
+    @Test
+    fun testRotate() {
+        val input = listOf(
+            charArrayOf('1', '2', '3'),
+            charArrayOf('4', '5', '6'),
+            charArrayOf('7', '8', '9')
+        )
+        val output = input.rotate()
+        assertThat(output).containsExactly(
+            charArrayOf('7', '4', '1'),
+            charArrayOf('8', '5', '2'),
+            charArrayOf('9', '6', '3')
+        )
+    }
+
+    @Test
+    fun testPuzzle2Example() {
+        assertEquals(64, calculateLoad(circle(exampleInput, 1000000000)))
     }
 
     companion object {
+        @JvmStatic
+        fun circle() = listOf(
+            Pair(1, """
+                .....#....
+                ....#...O#
+                ...OO##...
+                .OO#......
+                .....OOO#.
+                .O#...O#.#
+                ....O#....
+                ......OOOO
+                #...O###..
+                #..OO#....
+            """.trimIndent().lines().map { it.toCharArray() }),
+            Pair(2, """
+                .....#....
+                ....#...O#
+                .....##...
+                ..O#......
+                .....OOO#.
+                .O#...O#.#
+                ....O#...O
+                .......OOO
+                #..OO###..
+                #.OOO#...O
+            """.trimIndent().lines().map { it.toCharArray() }),
+            Pair(3, """
+                .....#....
+                ....#...O#
+                .....##...
+                ..O#......
+                .....OOO#.
+                .O#...O#.#
+                ....O#...O
+                .......OOO
+                #...O###.O
+                #.OOO#...O
+            """.trimIndent().lines().map { it.toCharArray() })
+        )
 
         val exampleInput = """
             O....#....
