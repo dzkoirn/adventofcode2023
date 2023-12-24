4099e6f721c21239c4ccc9803f24213e387171eb
Dzmitry Kalianchuk <koirn.dev@gmail.com>
2023-12-24 19:33:00 +0100

    Day 18: Puzzle 2 solved!

--- src/main/kotlin/adventofcode2023/day18/day18.kt
+++ src/main/kotlin/adventofcode2023/day18/day18.kt
@@ -1,35 +1,37 @@
 package adventofcode2023.day18
 
 import adventofcode2023.Point
-import adventofcode2023.line
 import adventofcode2023.col
+import adventofcode2023.line
 import adventofcode2023.readInput
+import java.awt.geom.Point2D
+import kotlin.math.abs
 import kotlin.math.max
 import kotlin.math.min
 import kotlin.time.measureTime
 
+
 data class Line(
     val start: Point,
-    val end: Point,
-    val color: String
+    val end: Point
 )
 
-fun makeLine(prevPoint: Point, direction: String, steps: Int, color: String): Line {
-    val newPoint = when(direction) {
+fun makeLine(prevPoint: Point, direction: String, steps: Int): Line {
+    val newPoint = when (direction) {
         "U" -> prevPoint.copy(first = prevPoint.line - steps)
         "R" -> prevPoint.copy(second = prevPoint.col + steps)
         "D" -> prevPoint.copy(first = prevPoint.line + steps)
         "L" -> prevPoint.copy(second = prevPoint.col - steps)
         else -> throw IllegalArgumentException("Could not handle $direction")
     }
-    return Line(prevPoint, newPoint, color)
+    return Line(prevPoint, newPoint)
 }
 
 fun parseInputToGraph(input: List<String>): List<Line> {
     val split = input.map { line -> line.split(' ') }
-    val firstLine = split.first().let { (dir, steps, color) -> makeLine(Point(0, 0), dir, steps.toInt(), color) }
-    return split.subList(1, split.size).fold(mutableListOf(firstLine)) { acc, (dir, steps, color)  ->
-        val newLine = makeLine(acc.last.end, dir, steps.toInt(), color)
+    val firstLine = split.first().let { (dir, steps, _) -> makeLine(Point(0, 0), dir, steps.toInt()) }
+    return split.subList(1, split.size).fold(mutableListOf(firstLine)) { acc, (dir, steps, _) ->
+        val newLine = makeLine(acc.last.end, dir, steps.toInt())
         acc.apply {
             add(newLine)
         }
@@ -59,8 +61,8 @@ fun visualizeGraph(polygon: List<Line>): String {
 
 fun isPointABorder(pLine: Int, pCol: Int, polygon: List<Line>): Boolean {
     return polygon.any { line ->
-        pLine in (min(line.start.line,line.end.line)..max(line.start.line,line.end.line)) &&
-        pCol in (min(line.start.col,line.end.col)..max(line.start.col, line.end.col))
+        pLine in (min(line.start.line, line.end.line)..max(line.start.line, line.end.line)) &&
+                pCol in (min(line.start.col, line.end.col)..max(line.start.col, line.end.col))
     }
 }
 
@@ -112,6 +114,59 @@ fun puzzle1(input: List<String>): Int {
     return countPointsInsidePolygon(graph)
 }
 
+fun parseHex(hex: String): Pair<String, Int> {
+    return hex.substring(hex.indexOf('#') + 1).let {
+        Pair(
+            when (val c = it.last()) {
+                '0' -> "R"
+                '1' -> "D"
+                '2' -> "L"
+                '3' -> "U"
+                else -> throw IllegalArgumentException("Illegal arrgument $c")
+            },
+            it.substring(0, 5).toInt(radix = 16)
+        )
+    }
+}
+
+fun parseInputToGraph2(input: List<String>): List<Line> {
+    val split = input.map { line -> line.split(' ') }
+    val firstLine = split.first().let { (_, _, hexNumber) ->
+        val (dir, steps) = parseHex(hexNumber.substring(hexNumber.indexOf('(') + 1, hexNumber.indexOf(')') ))
+        makeLine(Point(0, 0), dir, steps) }
+    return split.subList(1, split.size).fold(mutableListOf(firstLine)) { acc, (_, _, hexNumber)  ->
+        val (dir, steps) = parseHex(hexNumber.substring(hexNumber.indexOf('(') + 1, hexNumber.indexOf(')')))
+        val newLine = makeLine(acc.last.end, dir, steps)
+        acc.apply {
+            add(newLine)
+        }
+    }
+}
+
+/**
+ * Works like a charm!
+ * Thanks https://www.reddit.com/r/adventofcode/comments/18l2nk2/2023_day_18_easiest_way_to_solve_both_parts/ and
+ * https://github.com/wilkotom/Aoc2023/blob/main/day18/src/main.rs
+ */
+fun shoelaceFormula(lines: List<Line>): Long {
+    val perimeter = lines.fold(0L) { p, l -> p + max(abs(l.end.line - l.start.line), abs(l.end.col - l.start.col))}
+
+    val points = listOf(lines.first.start) + lines.map { it.end }
+
+    val area = abs(points.windowed(size = 2).fold(0L) { area, (p1, p2) ->
+        area + (p1.line.toLong() * p2.col.toLong() - p2.line.toLong() * p1.col.toLong())
+    } / 2)
+
+    return area + (perimeter / 2) + 1
+}
+
+
+
+fun puzzle2(input: List<String>): Long {
+    val graph = parseInputToGraph2(input)
+    return shoelaceFormula(graph)
+}
+
 fun main() {
     println("Day 18")
     val input = readInput("day18")
@@ -119,4 +174,8 @@ fun main() {
         println("Puzzle 1 ${puzzle1(input)}")
     }
     println("Puzzle 1 took $time1")
+    val time2 = measureTime {
+        println("Puzzle 2 ${puzzle2(input)}")
+    }
+    println("Puzzle 2 took $time2")
 }
--- src/test/kotlin/Day18Test.kt
+++ src/test/kotlin/Day18Test.kt
@@ -1,8 +1,12 @@
+import adventofcode2023.day18.puzzle2
+import adventofcode2023.day18.parseHex
 import adventofcode2023.day18.parseInputToGraph
 import adventofcode2023.day18.puzzle1
 import adventofcode2023.day18.visualizeGraph
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.MethodSource
 
 class Day18Test {
 
@@ -32,7 +36,39 @@ class Day18Test {
         assertEquals(62, puzzle1(exampleInput))
     }
 
+    @ParameterizedTest
+    @MethodSource("hexParseSource")
+    fun testParseHex(testCase: Pair<String, Pair<String, Int>>) {
+        assertEquals(testCase.second, parseHex(testCase.first))
+    }
+
+    @Test
+    fun testPuzzle2() {
+        val actual = puzzle2(exampleInput)
+        val expected = 952408144115
+        assertEquals(expected, actual)
+    }
+
     companion object {
+        @JvmStatic
+        fun hexParseSource() = listOf(
+            Pair("#70c710", Pair("R", 461937)),
+            Pair("#0dc571", Pair("D", 56407)),
+            Pair("#5713f0", Pair("R", 356671)),
+            Pair("#d2c081", Pair("D", 863240)),
+            Pair("#59c680", Pair("R", 367720)),
+            Pair("#411b91", Pair("D", 266681)),
+            Pair("#8ceee2", Pair("L", 577262)),
+            Pair("#caa173", Pair("U", 829975)),
+            Pair("#1b58a2", Pair("L", 112010)),
+            Pair("#caa171", Pair("D", 829975)),
+            Pair("#7807d2", Pair("L", 491645)),
+            Pair("#a77fa3", Pair("U", 686074)),
+            Pair("#015232", Pair("L", 5411)),
+            Pair("#7a21e3", Pair("U", 500254))
+        )
+
+
         val exampleInput = """
             R 6 (#70c710)
             D 5 (#0dc571)
